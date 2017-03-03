441b31d9051c982e43c0f68d9f7c046349d1e1d1
Yuunagi Kagami <Yuunagi Kagami>
2017-03-03 15:52:58 +0800

    Test 3 classes

--- src/test/java/seed/domain/ObjectiveRepositoryTest.java
+++ src/test/java/seed/domain/ObjectiveRepositoryTest.java
@@ -1,7 +1,116 @@
 package seed.domain;
+import org.bson.types.ObjectId;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import seed.Application;
+import seed.repository.ObjectiveListRepository;
+import seed.repository.ObjectiveRepository;
+import seed.repository.UserRepository;
+
+import java.time.DateTimeException;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Macsnow on 2017/3/2.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@SpringBootConfiguration
+
+
 public class ObjectiveRepositoryTest {
+    @Autowired
+    private ObjectiveRepository objectiveRepository;
+    private Objective objective1;
+    private Objective objective2;
+    private Objective objective3;
+    private Pageable pageable;
+    private ObjectId id1;
+    private ObjectId id2;
+    private ObjectId id3;
+    private ObjectId id4;
+    private ObjectId id5;
+    private ObjectId id6;
+    private DateTime id7;
+    private DateTime id8;
+
+
+    @Before
+    public void setUp() {
+        pageable = new PageRequest(0, 20);
+        objective1 = new Objective("Alan");
+        objective2 = new Objective();
+        objective3 = new Objective();
+        id1 = objectiveRepository.insert(objective1).getId();
+        id2 = objectiveRepository.insert(objective2).getId();
+        id3 = objectiveRepository.insert(objective1).getUserId();
+        id4 = objectiveRepository.insert(objective2).getUserId();
+        id5 = objectiveRepository.insert(objective1).getListIdId();
+        id6 = objectiveRepository.insert(objective2).getListIdId();
+        id7 = objectiveRepository.insert(objective1).getDeadline();
+        id8 = objectiveRepository.insert(objective2).getDeadline();
+        objective1.setPriority(1);
+        objective1.setStatus(true);
+
+
+    }
+    @Test
+    public void read(){
+        assertNotNull(objectiveRepository.findById(id1));
+        assertNotNull(objectiveRepository.findByUserId(id3,pageable));
+        assertNotNull(objectiveRepository.findByListId(id5,pageable));
+        assertNotNull(objectiveRepository.findByTitleIgnoreCase(objective1.getTitle(),pageable));
+        assertNotNull(objectiveRepository.findByDeadline(id7,pageable));
+        assertNotNull(objectiveRepository.findByPriority(objective1.getPriority(),pageable));
+        assertNotNull(objectiveRepository.findByStatus(objective1.getStatus(),pageable));
+        objectiveRepository.delete(id1);
+        objectiveRepository.delete(id2);
+        objectiveRepository.delete(id3);
+        objectiveRepository.delete(id4);
+        objectiveRepository.delete(id5);
+        objectiveRepository.delete(id6);
+
+
+    }
+
+    @Test
+    public void update(){
+        objective1.setId(id2);
+        assertEquals(id2,objectiveRepository.findById(objective1.getId()));
+        objective1.setUserId(id4);
+        assertEquals(id4,objectiveRepository.findByUserId(objective1.getUserId(),pageable));
+        objective1.setListId(id6);
+        assertEquals(id6,objectiveRepository.findByListId(objective1.getListIdId(),pageable));
+        objective1.setTitle("Aria");
+        assertEquals("Aria",objectiveRepository.findByTitleIgnoreCase(objective1.getTitle(),pageable));
+        objective1.setDeadline(id8);
+        assertEquals(id8,objectiveRepository.findByDeadline(objective1.getDeadline(),pageable));
+        objective1.setPriority(2);
+        assertEquals(2,objectiveRepository.findByPriority(objective1.getPriority(),pageable));
+        objective1.setStatus(false);
+        assertEquals(false,objectiveRepository.findByStatus(objective1.getStatus(),pageable));
+        objectiveRepository.delete(id1);
+        objectiveRepository.delete(id2);
+        objectiveRepository.delete(id3);
+        objectiveRepository.delete(id4);
+        objectiveRepository.delete(id5);
+        objectiveRepository.delete(id6);
+
+
+
+
+
+
+    }
 }
--- src/test/java/seed/domain/UserRepositoryTest.java
+++ src/test/java/seed/domain/UserRepositoryTest.java
@@ -1,7 +1,87 @@
 package seed.domain;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import seed.Application;
+import seed.repository.ObjectiveListRepository;
+import seed.repository.UserRepository;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Macsnow on 2017/3/2.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@SpringBootConfiguration
+
 public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    private User user1;
+    private User user2;
+    private User user3;
+    private Pageable pageable;
+    private ObjectId id1;
+    private ObjectId id2;
+    private ObjectId id3;
+
+    @Before
+    public void setUp(){
+        pageable = new PageRequest(0, 20);
+        user1 = new User("Alan","Alan","",true);
+        user1.setEmail("Alan@gmail.com");
+        user2 = new User();
+        user3 = new User();
+        id1 = userRepository.insert(user1).getId();
+        id2 = userRepository.insert(user2).getId();
+        id3 = userRepository.insert(user3).getId();
+
+    }
+
+    @Test
+    public void read(){
+        assertNotNull(userRepository.findById(id1));
+        assertNotNull(userRepository.findByEmail(user1.getEmail()));
+        assertNotNull(userRepository.findByOpenId(user1.getOpenId()));
+        assertNotNull(userRepository.findByUsername(user1.getUsername(),pageable));
+        userRepository.delete(id1);
+        userRepository.delete(id2);
+        userRepository.delete(id3);
+
+
+
+    }
+
+    @Test
+    public void update() {
+        user1.setId(id2);
+        assertEquals(id2,userRepository.findById(user1.getId()));
+        user1.setEmail("Aria@gmail.com");
+        assertEquals("Aria@gmail.com", userRepository.findByEmail(user1.getEmail()));
+        user1.setOpenId("Aria");
+        assertEquals("Aria", userRepository.findByOpenId(user1.getOpenId()));
+        user1.setUsername("Aria");
+        assertEquals("Aria", userRepository.findByUsername(user1.getUsername(),pageable));
+        userRepository.delete(id1);
+        userRepository.delete(id2);
+        userRepository.delete(id3);
+
+    }
+
+
+
+
+
+
+
 }
